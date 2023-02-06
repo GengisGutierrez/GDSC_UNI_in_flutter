fe16a94799b20b6610192c68269cc9b06d2b9f83
Gengis <gengis.gutierrez.c@gmail.com>
2023-02-06 15:23:51 -0500

    First commit, empezamos a codear

---
+++ android/app/src/main/kotlin/com/example/google_developer_student_clubs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.google_developer_student_clubs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_developer_student_clubs/screens/onboding/onboding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GDSC UNI',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFEEF1F8),
+        primarySwatch: Colors.blue,
+        fontFamily: "Mukta",
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
+        ),
+      ),
+      home: const OnbodingScreen(),
+    );
+  }
+  
+  static const defaultInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(16)),
+    borderSide: BorderSide(
+      color: Color(0xFFDEE3F2),
+      width: 1,
+    ),
+  );
+
+}
