a83d6556dde198f6f9ec83109b1dab5cb031081d
Gengis <gengis.gutierrez.c@gmail.com>
2023-02-06 19:08:35 -0500

    Completing Onboding

---
+++ lib/screens/onboding/onboding_screen.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OnbodingScreen extends StatefulWidget {
+  const OnbodingScreen({super.key});
+
+  @override
+  State<OnbodingScreen> createState() => _OnbodingScreenState();
+}
+
+class _OnbodingScreenState extends State<OnbodingScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
