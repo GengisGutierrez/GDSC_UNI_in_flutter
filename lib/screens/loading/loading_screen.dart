99142fc6e9cedae5968eddec5fc45a48f1216e8d
Gengis <gengis.gutierrez.c@gmail.com>
2023-02-08 00:06:28 -0500

    First version of SplashScreen with Rive (We need to use Lottie, will be better)

--- lib/screens/loading/loading_screen.dart
+++ lib/screens/loading/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_developer_student_clubs/screens/home/home_screen.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:rive/rive.dart';
 
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
@@ -13,10 +14,19 @@ class SplashScreenPage extends StatefulWidget {
 class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 3,
-      navigateAfterSeconds: new HomeScreen(),
-      useLoader: false,
+    return AnimatedSplashScreen(
+      duration: 3000,
+      splash: Container(
+        child: Center(
+          child: SizedBox(
+              child: RiveAnimation.asset('assets/RiveAssets/gdsc_uni_logo.riv')
+          ),
+        ),
+      ),
+      nextScreen: HomeScreen(),
     );
   }
+
 }
+
+
